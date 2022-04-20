9dcecd9554b22fd9c808c1a23e7cb3855f75cfb0
vuhai2310 <lvhai.19it4@vku.udn.vn>
2022-04-20 11:54:25 +0700

    13 - Update Overview content

--- src/components/Tabs/TabContents/Overview/Overview.jsx
+++ src/components/Tabs/TabContents/Overview/Overview.jsx
@@ -14,7 +14,7 @@ const Overview = () => {
         <h2 className="overview__title">Attack On Titan Final Season</h2>
         <div className="overview__subtitle">
           <span>Update at 23:10 every Saturday</span>
-          <span className="overview__subtitle__divide-line"></span>
+          <span className="overview__divide-line"></span>
           <span className="overview__subtitle__view">2310K Views</span>
           <div className="overview__subtitle__tag-genre">
             <span className="tag-genre__txt">Action</span>
@@ -39,19 +39,32 @@ const OverviewContent = (props) => {
   return (
     <>
       <div className={`overview__content ${props.cName}`}>
-        <p>
-          Twilight, or "Loid Forger," quickly adopts the unassuming orphan Anya
-          to play the role of a six-year-old daughter and prospective Eden
-          Academy student. For a wife, he comes across Yor Briar, an
-          absent-minded office worker who needs a pretend partner of her own to
-          impress her friends. However, Loid is not the only one with a hidden
-          nature. Yor moonlights as the lethal assassin "Thorn Princess." For
-          her, marrying Loid creates the perfect cover. Meanwhile, Anya is not
-          the ordinary girl she appears to be; she is an esper, the product of
-          secret experiments that allow her to read minds. Although she uncovers
-          their true identities, Anya is thrilled that her new parents are cool
-          secret agents! She would never tell them, of course. That would ruin
-          the fun.
+        <div className="overview__content__info--row">
+          <span>Anime</span>
+          <span className="overview__divide-line"></span>
+          <span>Japan</span>
+        </div>
+        <div className="overview__content__info--row">
+          <span>Starting: 23/10/2001</span>
+        </div>
+        <div className="overview__content__info--row">
+          <span>Full</span>
+        </div>
+        <div className="overview__content__info--row">
+          <span style={{ marginRight: 5, color: "#666" }}>Initial</span>
+          <span>進撃の巨人 The Final Season part 2</span>
+        </div>
+        <p className="overview__content__desc">
+          The story of "Attack on Giant" describes that a hundred years ago,
+          mysterious giant monsters appeared inexplicably in the world, and they
+          continued to cruel and cannibalized. Humans could hardly fight against
+          them. Finally, humans built three towering walls with a width of 3
+          meters and a height of 50 meters. There has not been a giant attack
+          for a hundred years. People have become accustomed to the life inside
+          the wall. They are numb to peace, afraid of adventure and change.
+          Giants flooded into the town. 10-year-old Allen saw the tragedy of her
+          mother being bitten by the giant. Allen vowed to destroy all the
+          giants.
         </p>
       </div>
     </>
