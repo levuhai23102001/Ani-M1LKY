13866224fc97ad7b045dcf0937f47ae1280d49ff
vuhai2310 <lvhai.19it4@vku.udn.vn>
2022-05-01 22:54:37 +0700

    16 - Update Progress bar

--- src/pages/Watch/Player.jsx
+++ src/pages/Watch/Player.jsx
@@ -22,18 +22,38 @@ const Player = () => {
   //states
   const [isPlaying, setIsPlaying] = useState(false);
   const [showReplay, setShowReplay] = useState(false);
-
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
   //references
   const videoRef = useRef();
+  const progressRef = useRef();
+  const animationRef = useRef();
+
   //Play & Pause
   const handlePlayPause = () => {
     const prevValue = isPlaying;
     setIsPlaying(!prevValue);
     if (!prevValue) {
       videoRef.current.play();
+      animationRef.current = requestAnimationFrame(whilePlaying);
     } else {
       videoRef.current.pause();
+      cancelAnimationFrame(animationRef.current);
     }
+  };
+  //While Playing
+  const whilePlaying = () => {
+    progressRef.current.value = videoRef.current.currentTime;
+    changePlayerCurrentTime();
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  };
+  //Change player current time
+  const changePlayerCurrentTime = () => {
+    progressRef.current.style.setProperty(
+      "--seek-before-width",
+      `${(progressRef.current.value / duration) * 100}%`
+    );
+    setCurrentTime(progressRef.current.value);
   };
   // Ended Video
   const onEnded = () => {
@@ -44,6 +64,29 @@ const Player = () => {
     videoRef.current.play();
   };
 
+  //Duration
+  useEffect(() => {
+    const times = Math.floor(videoRef.current.duration);
+    setDuration(times);
+    progressRef.current.max = times;
+  }, [videoRef?.current?.loadedmetadata, videoRef?.current?.readyState]);
+
+  //Format time video
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const reMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const reSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${reMinutes}:${reSeconds}`;
+  };
+
+  //Current time video
+  const changeRange = () => {
+    videoRef.current.currentTime = progressRef.current.value;
+    changePlayerCurrentTime();
+  };
+
+  // Zoom out
   const handleZoomOut = () => {
     videoRef.current.requestFullscreen();
   };
@@ -88,18 +131,16 @@ const Player = () => {
           <div className="ani-player">
             <div className="ani-player__controls">
               <div className="ani-player__progress">
-                {/* <div className="progress-bar"></div> */}
-                <progress className="progress-bar" min="0" max="100"></progress>
                 <input
                   className="seek-bar"
-                  min="0"
-                  max="100"
+                  defaultValue="0"
                   type="range"
                   step="1"
+                  ref={progressRef}
+                  onChange={changeRange}
                 ></input>
               </div>
               <div className="ani-player__left">
-                {/* ended show replay button */}
                 {!showReplay ? (
                   <div
                     className="ani-player__btn play-pause-control"
@@ -130,9 +171,13 @@ const Player = () => {
                   />
                 </div>
                 <div className="ani-player__timer">
-                  <span className="time-elapsed">00:00</span>
+                  <span className="time-elapsed">
+                    {calculateTime(currentTime)}
+                  </span>
                   <span> / </span>
-                  <span className="duration">00:00</span>
+                  <span className="duration">
+                    {duration && !isNaN(duration) && calculateTime(duration)}
+                  </span>
                 </div>
                 {/* <div className="ani-player__btn volume-mute-control">
                   <VolumeMuteRoundedIcon fontSize="medium" />
